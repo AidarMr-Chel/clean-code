9d4c6505351093e31dfa092c410caad886ccf9c6
Aidarchik <garifullinajdar7@gmail.com>
2024-12-16 13:05:04 +0300

    Md Processor

--- Markdown/Markdown/MdProcessor.cs
+++ Markdown/Markdown/MdProcessor.cs
@@ -1,7 +1,35 @@
-﻿namespace Markdown
-{
-    public class MdProcessor
-    {
+﻿using System.Text;
 
+namespace Markdown
+{
+    public static class MdProcessor
+    {
+        public static string Render(string textInMd)
+        {
+            if (string.IsNullOrWhiteSpace(textInMd))
+                throw new ArgumentException("Input text cannot be null or empty.", nameof(textInMd));
+
+            var renderedText = new StringBuilder();
+            foreach (var paragraph in GetParagraphs(textInMd))
+            {
+                var tags = TagParser.BuildTags(paragraph);
+                var renderedParagraphs = MdToHtml.Convert(paragraph, tags.PairTags, tags.SingleTags);
+                renderedText.Append(renderedParagraphs);
+            }
+
+            return renderedText.ToString();
+        }
+
+        private static IEnumerable<string> GetParagraphs(string text)
+        {
+            var paragraphs = text.Split(Environment.NewLine, StringSplitOptions.None);
+            foreach (var paragraph in paragraphs)
+            {
+                if (!string.IsNullOrWhiteSpace(paragraph))
+                {
+                    yield return paragraph.Trim();
+                }
+            }
+        }
     }
 }
---
+++ Markdown/Testing/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Markdown;
+
+namespace Testing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(MDProcessor.Render("_Вот это_"));
+            
+        }
+    }
+}
